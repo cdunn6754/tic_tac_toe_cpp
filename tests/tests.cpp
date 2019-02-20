fceeda36aca2395dcbd255e74b7050189768331c
Clint <cdunn6754@gmail.com>
2019-02-20 16:50:50 -0700

    Moves board and player to a library adds tests
    I moved the ttt_board and ttt_player to their own libraries.
    This commit also finishes the implementation of make_move for
    ttt_board and tests.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,8 +1,35 @@
 #include "gtest/gtest.h"
 
+#include "ttt_board.h"
+#include "player.h"
 
-TEST (DummyTest, firstone) {
-  EXPECT_EQ (18.0, 19.0);
+
+TEST (BoardTest1, initialization) {
+  ttt_board test_board;
+  for (const player& p : test_board.get_state()) {
+      EXPECT_EQ (p, player::E);
+      EXPECT_NE (p, player::X);
+      EXPECT_NE (p, player::O);
+  }
+}
+
+TEST (BoardTest2, make_move) {
+  ttt_board test_board;
+  test_board.make_move(player::O, 3);
+  board_array state(test_board.get_state());
+  for (std::size_t  idx(0); idx<state.size(); idx++) {
+    player p = state[idx];
+    if (idx == 3) {
+      EXPECT_EQ (p, player::O);
+      EXPECT_NE (p, player::X);
+      EXPECT_NE (p, player::E);
+    }
+    else {
+      EXPECT_EQ (p, player::E);
+      EXPECT_NE (p, player::X);
+      EXPECT_NE (p, player::O);
+    }
+  }
 }
 
 

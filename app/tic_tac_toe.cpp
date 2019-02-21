9df4805c7f30781a8702e158f9fcdbadf17f4298
Clint <cdunn6754@gmail.com>
2019-02-21 12:35:43 -0700

    Completes the ttt app

--- app/tic_tac_toe.cpp
+++ app/tic_tac_toe.cpp
@@ -6,41 +6,38 @@
 int main() {
   
   ttt_board board;
+  
+  char winner = board.check_winner();
+  bool x_turn = true;
+  player current_player;
 
-  while (!board.check_finished()) {
+  while (winner=='N') {
     int move_loc;
-
+    current_player = x_turn ? player::X : player::O;
+    
     std::cout << std::endl;
     board.print_board();
     // X player move
     do {
-      std::cout << "X player move: ";
+      std::cout << player_print(current_player) <<" player move: ";
       std::cin >> move_loc;
       std::cout << std::endl;
-    } while (!board.make_move(player::X, move_loc));
+    } while (!board.make_move(current_player, move_loc));
 
     std::cout << std::endl;
-    board.print_board();
-        
-    // O player move
-    do {
-      std::cout << "O player move: ";
-      std::cin >> move_loc;
-      std::cout << std::endl;
-    } while (!board.make_move(player::O, move_loc));
-
+    
+    x_turn = !x_turn;
+    winner = board.check_winner();
   }
   
   std::cout << std::endl;
   board.print_board();
   
-  char winner = board.check_winner();
-  
   if (winner == 'D') {
-    std::cout << "The game was a draw" << std::endl;
+    std::cout << "The game was a draw!" << std::endl;
   }
   else {
-    std::cout << "Player " << winner << "won this game." << std::endl;
+    std::cout << "Player " << winner << " won this game!" << std::endl;
   }
   
   return 0;

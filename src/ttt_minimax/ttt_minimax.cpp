6615cce04f1eed6cddc7ee8a6a06e36558688502
Clint <cdunn6754@gmail.com>
2019-02-22 09:50:42 -0700

    Adds the basic skeleton for minimax files

---
+++ src/ttt_minimax/ttt_minimax.cpp
@@ -0,0 +1,14 @@
+#include "ttt_minimax.h"
+#include <array>
+
+// Very similar to board_array from tt_board but
+// contains native chars rather than player types.
+// This function can be used by passing normal arrays.
+// So hopefully I can call this from Python.
+typedef std::array<char, 9> native_board_array;
+
+native_board_array ttt_minimax(native_board_array state){
+  
+  
+  return state;
+}
---
+++ src/ttt_minimax/ttt_minimax.h
@@ -0,0 +1,5 @@
+#include "player.h"
+#include "ttt_board.h"
+
+
+board_array ttt_minimax(board_array i_state);
